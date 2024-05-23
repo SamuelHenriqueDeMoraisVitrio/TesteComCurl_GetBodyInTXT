be4db2d50fdb3b9c6e6aeddd7de6090ad4f8df8b
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-05-23 01:09:06 -0300

    Testando Guardar em FILE

--- main.c
+++ main.c
@@ -2,26 +2,31 @@
 
 int main(int argc, char *argv[]) {
   CURL *curl;
-  CURLcode *rest;
-
-  FILE *arqTXT = fopen("texto/texto.txt", "w");
+  CURLcode res;
 
   curl_global_init(CURL_GLOBAL_ALL);
 
   curl = curl_easy_init();
   if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com");
+    FILE *arqTXT = fopen("texto/main.html", "w");
 
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, arqTXT);
 
-    curl_easy_perform(curl);
+    res = curl_easy_perform(curl);
+    fclose(arqTXT);
+
+    if (res != CURLE_OK) {
+      char *INFOCURL = NULL;
+      curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &INFOCURL);
+      fprintf(stderr, "\n\tErro ao executar sessão %s:\n\t%s\n", INFOCURL, curl_easy_strerror(res));
+      return 1;
+    }
 
     curl_easy_cleanup(curl);
   }
 
   curl_global_cleanup();
-
-  fclose(arqTXT);
 
   return 0;
 }
