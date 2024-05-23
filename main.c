c70595bf22613ed1b136b15ed7258c184da425ae
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-05-23 03:12:06 -0300

    usando json com curl

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <curl/curl.h>
 
+#include "CxpathJson.h"
+
+CxpathJsonNamespace js;
+CxpathJsonErrorNamespace errors;
+
+size_t lendo_ids(CxpathJson *json, size_t size, size_t num_size, FILE *arqTXT) {
+  if (errors.has_errors(json)) {
+    char *message = errors.get_error_message(json);
+    int code = errors.get_error_code(json);
+    printf("code: %d\n", code);
+    printf("message: %s\n", message);
+    js.free(json);
+    fprintf(stderr, "Erro\n");
+  }  // if errors
+
+  for (size_t i = 0; i < js.size(json, "[]"); i++) {
+    int id = js.get_int(json, "[%d, 'id']", i);
+    if (errors.has_errors(json)) {
+      char *message = errors.get_error_message(json);
+      int code = errors.get_error_code(json);
+      printf("code: %d\n", code);
+      printf("message: %s\n", message);
+      js.free(json);
+      fprintf(stderr, "Erro\n");
+    }  // if errors
+    fprintf(arqTXT, "%d\n", id);
+
+  }  // for json
+
+  js.free(json);
+
+  return size * num_size;
+}  // function
+
 int main(int argc, char *argv[]) {
+  js = newCxpathJsonNamespace();
+  errors = js.errors;
+
   CURL *curl;
   CURLcode res;
 
@@ -8,8 +45,11 @@ int main(int argc, char *argv[]) {
 
   curl = curl_easy_init();
   if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com");
-    FILE *arqTXT = fopen("texto/main.html", "w");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts");
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, lendo_ids);
+
+    FILE *arqTXT = fopen("texto/texto.txt", "wb");
 
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, arqTXT);
 
